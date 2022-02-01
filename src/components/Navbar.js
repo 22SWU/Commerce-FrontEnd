b01fee71d2cd39ed7c2efe96515b1873378d4f41
naerim <naerim1119@gmail.com>
2022-02-02 00:57:19 +0900

    project setting

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Navbar = () => {
+  return <Container>navbar</Container>;
+};
+
+const Container = styled.div`
+  height: 60px;
+  background-color: black;
+`;
+
+export default Navbar;
