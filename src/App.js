c59689375b7f955468b785e77f61f38a63570c9b
naerim <naerim1119@gmail.com>
2022-02-05 19:20:03 +0900

    router v6

--- src/App.js
+++ src/App.js
@@ -4,9 +4,31 @@ import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+// react-router-dom 버전 바뀜
+// Switch -> Routes, component -> element
+// Redirect -> Navigate
 const App = () => {
-  return <Home />;
+  const user = true;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route
+          path="/login"
+          element={user ? <Navigate replace to="/" /> : <Login />}
+        />
+        <Route
+          path="/register"
+          element={user ? <Navigate replace to="/" /> : <Register />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
