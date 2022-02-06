26c5173ff12efaf027e3d2a49db09775cef5cc40
naerim <naerim1119@gmail.com>
2022-02-06 14:55:45 +0900

    fetch filter products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,16 +1,66 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { popularProducts } from "../data";
 import Product from "./Product";
 
-const Products = () => {
+const Products = ({ cat, filters, sort }) => {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          cat
+            ? `http://localhost:5000/api/products?category=${cat}`
+            : "http://localhost:5000/api/products"
+        );
+        setProducts(res.data);
+      } catch (err) {}
+    };
+    getProducts();
+  }, [cat]);
+
+  useEffect(() => {
+    cat &&
+      setFilteredProducts(
+        products.filter((item) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value)
+          )
+        )
+      );
+  }, [products, cat, filters]);
+
+  useEffect(() => {
+    if (sort === "newest") {
+      setFilteredProducts(
+        (prev) => [...prev].sort((a, b) => a.createdAt - b.createdAt) // 최신순
+      );
+    } else if (sort === "asc") {
+      setFilteredProducts(
+        (prev) => [...prev].sort((a, b) => a.price - b.price) // 가격 낮은 순
+      );
+    } else {
+      setFilteredProducts(
+        (prev) => [...prev].sort((a, b) => b.price - a.price) // 가격 높은 순
+      );
+    }
+  }, [sort]);
+
   return (
     <Container>
-      {popularProducts.map((item) => (
-        <Product item={item} key={item.id} />
-      ))}
+      {cat
+        ? filteredProducts.map((item) => <Product item={item} key={item.id} />)
+        : products
+            .slice(0, 8)
+            .map((item) => <Product item={item} key={item.id} />)}
     </Container>
   );
 };
+
+// .slice(0,8) maximum 8개
 
 const Container = styled.div`
   padding: 20px;
