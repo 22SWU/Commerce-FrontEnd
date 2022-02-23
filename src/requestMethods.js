0ca99011dcf501fdf93c419022ba8f2817020b71
naerim <naerim1119@gmail.com>
2022-02-23 14:05:48 +0900

    accessToken - frontend

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZmQ0MzA4ZGZjMjA3MTE3YjEzZmE4MCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NDEyNzM5OCwiZXhwIjoxNjQ0Mzg2NTk4fQ.4OgPLjCsr6L0ugvkHbZErT20NKOW3gsZinzAs-uNkX8";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
+  .currentUser.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
